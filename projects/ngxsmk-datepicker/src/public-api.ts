c35d3648b5a34da05453f80d314fc39ce6cfdfbc
Tomasz <tomek.mejer@protonmail.com>
2025-10-28 10:42:31 +0100

    Standalone build branch

--- projects/ngxsmk-datepicker/src/public-api.ts
+++
@@ -1,8 +0,0 @@
-/*
- * Public API Surface of ngxsmk-datepicker
- */
-
-export * from './lib/ngxsmk-datepicker';
-export * from './lib/components/custom-select.component';
-export * from './lib/utils/date.utils';
-export * from './lib/utils/calendar.utils';
